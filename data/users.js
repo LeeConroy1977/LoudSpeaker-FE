a47db88f8a533f1f9a198570e2f83cb1a0353bf9
LeeConroy1977 <leejconroy@gmail.com>
2024-07-02 12:20:03 +0100

    Initial folder set-up, tailwind installed, dummy data added

---
+++ data/users.js
@@ -0,0 +1,40 @@
+const users = [
+  {
+    username: "tickle122",
+    name: "Tom Tickle",
+    avatar_url:
+      "https://images.unsplash.com/photo-1527980965255-d3b416303d12?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YXZhdGFyfGVufDB8fDB8fHww",
+  },
+  {
+    username: "grumpy19",
+    name: "Paul Grump",
+    avatar_url:
+      "https://images.unsplash.com/photo-1628157588553-5eeea00af15c?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    username: "happyamy2016",
+    name: "Amy Happy",
+    avatar_url:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    username: "cooljmessy",
+    name: "Peter Messy",
+    avatar_url:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    username: "weegembump",
+    name: "Gemma Bump",
+    avatar_url:
+      "https://images.unsplash.com/photo-1607746882042-944635dfe10e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    username: "jessjelly",
+    name: "Jess Jelly",
+    avatar_url:
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=1961&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+export default users;
